d662da4680742ce5a28820be867653f1ad791a09
FernandoUsuario <Usuario@DESARROLLO>
2022-06-18 19:49:39 -0400

    Con despiegue de Semana

--- Reservas/Controllers/ReservaController.cs
+++ Reservas/Controllers/ReservaController.cs
@@ -15,42 +15,28 @@ namespace Reservas.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            Array lista;
             int lab = 1;
-            string[,] semana = new String[13, 7];
             DateTime fecha = DateTime.Now, primerdia = fecha.AddDays(-(byte)fecha.DayOfWeek + 1)
             , ultimodia = fecha.AddDays(6 - (byte)fecha.DayOfWeek);
+
+            var res = _context.TbReservas.Include(b => b.IdUsrNavigation)
+                                         .Include(b => b.IdModuloNavigation)
+                                         .Include(b => b.IdLabNavigation);
             using (var context = _context)
             {
-                var lst = from d in _context.TbReservas
+                var lst = from d in res
                           where d.FechaReserva >= primerdia &&
                           d.FechaReserva <= ultimodia &&
                           d.IdLab == lab
-
                           select d;
-                if (lst.Count()>0)
-                {
-                    TbReserva oReserva = lst.First();
-                    lista = lst.ToArrayAsync().Wait();
-                    for (int i = 1; i <= lst.Count(); i++)
-                    {
-                        
 
-                    }
-                }
+                return View(await lst.ToListAsync());
             }
 
-            var res = _context.TbReservas.Include(b => b.IdUsrNavigation)
-                .Include(b => b.IdModuloNavigation)
-                .Include(b => b.IdLabNavigation);
+        }
 
-            return View(await res.ToListAsync());
-        }
-                
-        }
         public IActionResult Create()
         {
-            ViewData["Modulos"] = new SelectList(_context.TbReservas, "IdModulo", "InicioMod", "FinMod");
 
             return View();
         }
