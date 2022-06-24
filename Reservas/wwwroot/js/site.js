3e7da8333ab441722d60524d17d726db285722e0
FernandoUsuario <Usuario@DESARROLLO>
2022-06-23 20:53:07 -0400

    Prueba botonos dialog

--- Reservas/wwwroot/js/site.js
+++ Reservas/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(function ($) {
+    'use strict';
+
+    var $accountDelete = $('#delete-dialogo'),
+        $accountDeleteDialog = $('#confirm-dialogo');
+
+    $accountDelete.on('click', function () {
+        $accountDeleteDialog[0].showModal();
+    });
+
+    $('#Cancelar').on('click', function () {
+        $accountDeleteDialog[0].close();
+    });
+    $('#Guardar').on('click', function () {
+        $accountDeleteDialog[0].close();
+    });
+
+
+})(jQuery);
+(function ($) {
+    'use strict';
+
+    var $accountDelete = $('#delete2-dialogo'),
+        $accountDeleteDialog = $('#confirm-dialogo');
+
+    $accountDelete.on('click', function () {
+        $accountDeleteDialog[0].showModal();
+    });
+
+    $('#Cancelar').on('click', function () {
+        $accountDeleteDialog[0].close();
+    });
+    $('#Guardar').on('click', function () {
+        $accountDeleteDialog[0].close();
+    });
+
+
+})(jQuery);
